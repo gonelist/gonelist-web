7466a13ed422ad92f47b906f50495ceb89c96934
sillywa <1909278528@qq.com>
2020-04-10 22:46:53 +0800

    request url改为 window.location.origin

--- project.config.js
+++ project.config.js
@@ -1,6 +1,7 @@
+
 module.exports = {
-  // 后端接口
-  baseURL: "https://goindex.cugxuan.cn:8001/",
+  // 后端接口，已弃用
+  // baseURL: "https://goindex.cugxuan.cn:8001/",
   // 网站名称
   title: "GOindex-Web",
   // 前端项目打包部署在根目录下，不建议修改，详情见readme
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 const ProjectConfig = require("../../project.config")
-
+const baseURL = window.location.origin
 // 创建实例时设置配置的默认值
 var service = axios.create({
-  baseURL: ProjectConfig.baseURL,
+  baseURL: baseURL,
   timeout: 15000 
 });
 // 添加响应拦截器
@@ -15,7 +15,7 @@ service.interceptors.response.use(
       console.log("请求成功");
       return res;
     } else if (res.code == 400) {
-      window.location.href = `${ProjectConfig.baseURL}/login`
+      window.location.href = `${baseURL}/login`
     } else {
       alert("请求失败");
     }
