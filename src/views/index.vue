055350f7d643be81baa24eb92587e69f977b0ebc
青木 <authemail@qq.com>
2020-07-20 14:42:22 +0800

    修复控制台警告问题

--- src/views/index.vue
+++ src/views/index.vue
@@ -222,7 +222,7 @@
       <input v-model="pass" type="password" placeholder="请输入密码" />
       <p style="color: red" v-show="pass_count > 1">密码错误!</p>
     </Modal>
-    <Modal v-model="img_modal" title="图片预览" footer-hide="true">
+    <Modal v-model="img_modal" title="图片预览" :footer-hide="true">
       <img :src="img_src" alt="" style="width:100%;height:100%" />
     </Modal>
   </div>
