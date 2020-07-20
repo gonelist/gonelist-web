7f7e27cebf1dc999bf8cca9fcd0c8468860487fa
StringKE <31089228+StringKe@users.noreply.github.com>
2020-07-20 15:31:41 +0800

    修复无法播放，控制台警告，加入编码规范检查 (#10)
    * 修改成 yarn 管理，整体项目
    
    * 添加规范 eslint prettier 检查
    
    * 格式化所有文件，配置规范检查。
    
    * 修复点击无法播放问题
    
    * 修复控制台警告问题

--- src/components/Dplayer.vue
+++ src/components/Dplayer.vue
@@ -1,137 +1,150 @@
 <template>
   <!-- <div class="player-content">
-    <div class="player">
-      <div class="close"><span @click="close"> 关闭</span></div>
-      
-    </div>
-  </div> -->
+        <div class="player">
+          <div class="close"><span @click="close"> 关闭</span></div>
+
+        </div>
+      </div> -->
 
   <div ref="dplayer"></div>
-  
 </template>
 <script>
-  import DPlayer from 'dplayer';
+import DPlayer from "dplayer";
 
-  export default {
-    data() {
-      return {
-        dp: null,
-        options: {
-          autoplay: false,
-          theme: "#b7daff",
-          loop: false,
-          screenshot: false,
-          hotkey: true,
-          preload: 'auto',
-          volume: 0.7
-        },
-        name: ""
-      }
-    },
-    methods: {
-      play(video) {
-        console.log("现在播放：",video.playurl)
-        this.name = video.name
-        this.closeNotice()
+export default {
+  data() {
+    return {
+      dp: undefined,
+      options: {
+        autoplay: false,
+        theme: "#b7daff",
+        loop: false,
+        screenshot: false,
+        hotkey: true,
+        preload: "auto",
+        volume: 0.7
+      },
+      name: ""
+    };
+  },
+  mounted() {
+    if (!this.dp) {
+      this.$nextTick(() => {
         this.dp = new DPlayer({
-            container: this.$refs.dplayer,
-            ...this.options,
-            video: {
-                url: video.url,
-            }
-        });
-        this.$Notice.info({
-          name: "video",
-          title: "视频",
-          //desc: `正在播放${audio.name}，点击右上角可关闭`,
-          duration: 0,
-          render: h => {
-              return h('span', [
-                  '正在播放视频',
-                  h('strong', this.name),
-                  h('p',{
-                    style: "paddingTop:10px"
-                  },'点击右上角可关闭')
-              ])
-          },
-          onClose: () => {
-            this.close()
+          container: this.$refs.dplayer,
+          ...this.options,
+          video: {
+            type: "auto"
           }
         });
-        this.dp.on('error',() => {
-          this.closeNotice()
-          this.close()
-          this.$Message.error('视频播放出错，请重试');
-        });
-        this.dp.play()
-      },
-      switch(video) {
-        this.name = video.name
-       
-        this.dp.switchVideo({
-          url: video.playurl
-        })
-        this.play(video)
-        
-      },
-      close() {
-        this.closeNotice()
-        this.dp.pause()
-        //this.dp.destroy()
-        this.$emit("closeVideo")
-      },
-      closeNotice() {
-        this.$Notice.close("video")
-      }
+      });
+    }
+  },
+  methods: {
+    play(video) {
+      console.log("现在播放：", video.playUrl);
+      this.name = video.name;
+      this.closeNotice();
+      this.dp.switchVideo({
+        url: video.playUrl
+        // pic: 'second.png',
+        // thumbnails: 'second.jpg',
+      });
+      this.$Notice.info({
+        name: "video",
+        title: "视频",
+        //desc: `正在播放${audio.name}，点击右上角可关闭`,
+        duration: 0,
+        render: h => {
+          return h("span", [
+            "正在播放视频",
+            h("strong", this.name),
+            h(
+              "p",
+              {
+                style: "paddingTop:10px"
+              },
+              "点击右上角可关闭"
+            )
+          ]);
+        },
+        onClose: () => {
+          this.close();
+        }
+      });
+      this.dp.on("error", () => {
+        this.closeNotice();
+        this.close();
+        this.$Message.error("视频播放出错，请重试");
+      });
+      this.dp.play();
+    },
+    switch(video) {
+      this.name = video.name;
+
+      this.dp.switchVideo({
+        url: video.playurl
+      });
+      this.play(video);
+    },
+    close() {
+      this.closeNotice();
+      this.dp.pause();
+      //this.dp.destroy()
+      this.$emit("closeVideo");
+    },
+    closeNotice() {
+      this.$Notice.close("video");
     }
   }
-
+};
 </script>
 <style>
 /* .player-content {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 9;
-  background: rgba(128,128,128,0.48);
-}
-.player {
-    width: 80%;
-    height: 450px;
-    position: absolute;
-    left: 10%;
-    top: 50%;
-    margin: -225px 0 0 0;
-}
-.close {
-  text-align: right;
-  height:50px;
-  box-sizing: border-box;
-  line-height: 50px;
-  font-weight: bold;
-  font-size: 18px;
-}
-.close span {
-  cursor: pointer;
-}
-.dplayer {
-    height: 400px;
-} */
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          z-index: 9;
+          background: rgba(128,128,128,0.48);
+        }
+        .player {
+            width: 80%;
+            height: 450px;
+            position: absolute;
+            left: 10%;
+            top: 50%;
+            margin: -225px 0 0 0;
+        }
+        .close {
+          text-align: right;
+          height:50px;
+          box-sizing: border-box;
+          line-height: 50px;
+          font-weight: bold;
+          font-size: 18px;
+        }
+        .close span {
+          cursor: pointer;
+        }
+        .dplayer {
+            height: 400px;
+        } */
 .dplayer {
   position: fixed !important;
   left: 0;
   bottom: 0;
   height: 300px;
-  
 }
-@media (max-width: 768px) { 
+
+@media (max-width: 768px) {
   .dplayer {
     width: 100%;
   }
 }
-@media (min-width: 769px) { 
+
+@media (min-width: 769px) {
   .dplayer {
     width: 600px;
   }
--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
-import "font-awesome/css/font-awesome.min.css"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import "font-awesome/css/font-awesome.min.css";
 
-import { Message, Table, Notice, Icon, Modal, Input } from 'view-design'
-Vue.prototype.$Message = Message
-Vue.prototype.$Notice = Notice
-Vue.component('Table', Table)
-Vue.component('Icon', Icon)
-Vue.component('Input', Input)
-Vue.component('Modal', Modal)
-Vue.config.productionTip = false
+import { Message, Table, Notice, Icon, Modal, Input } from "view-design";
+Vue.prototype.$Message = Message;
+Vue.prototype.$Notice = Notice;
+Vue.component("Table", Table);
+Vue.component("Icon", Icon);
+Vue.component("Input", Input);
+Vue.component("Modal", Modal);
+Vue.config.productionTip = false;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // 为生产环境配置...
-  let startApp = function () {
-    axios.get('serverconfig.json').then((res) => {
-      console.log(res)
+  let startApp = function() {
+    axios.get("serverconfig.json").then(res => {
+      console.log(res);
       // 基础地址
       Vue.prototype.baseURL = res.data.baseURL;
       new Vue({
         router,
         store,
         render: h => h(App)
-      }).$mount('#app')
-    })
-  }
-  
-  startApp()
+      }).$mount("#app");
+    });
+  };
+
+  startApp();
 } else {
   // 为开发环境配置...
   Vue.prototype.baseURL = "https://gonelist.cugxuan.cn";
@@ -37,8 +37,5 @@ if (process.env.NODE_ENV === 'production') {
     router,
     store,
     render: h => h(App)
-  }).$mount('#app')
+  }).$mount("#app");
 }
-
-
-
