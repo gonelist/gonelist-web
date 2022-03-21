e7ea739ac46acd01740719125bf449ff250920a0
3343780376 <3343780376@qq.com>
2022-03-21 22:57:57 +0800

    适配后端权限系统，增加功能，上传文件（包括大文件），创建文件夹，删除文件

--- src/views/index.vue
+++ src/views/index.vue
@@ -73,12 +73,27 @@
               @keyup.native="search"
               :key="'input-search'"
             />
+            <br />
             <Button
-              v-if="site_config.upload"
+              v-if="site_config.upload && !site_config.admin_permission"
+              class="file-upload"
+              type="info"
+              @click="update_permission"
+              >权限提升
+            </Button>
+            <Button
+              v-if="site_config.admin_permission"
               class="file-upload"
               type="info"
               @click="upload"
               >文件上传
+            </Button>
+            <Button
+              v-if="site_config.admin_permission"
+              class="file-upload"
+              type="info"
+              @click="mkdir"
+              >文件夹创建
             </Button>
           </div>
         </div>
@@ -128,6 +143,41 @@
             <template slot-scope="{ row }" slot="size">
               <div style="width: max-content">{{ row.size | formatSize }}</div>
             </template>
+            <template
+              slot="delete"
+              v-if="site_config.admin_permission"
+              slot-scope="{ row }"
+            >
+              <span @click="delete_file(row.path, row.name)">
+                <svg
+                  x="1647855783671"
+                  class="icon"
+                  viewBox="0 0 1024 1024"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  p-id="2224"
+                  width="20"
+                  height="20"
+                >
+                  <path
+                    d="M909.050991 169.476903l-217.554898 0 0-31.346939c0-39.5866-32.205493-71.792093-71.793116-71.792093L408.15591 66.337871c-39.5866 0-71.792093 32.205493-71.792093 71.792093l0 31.346939L113.349581 169.476903c-11.013845 0-19.942191 8.940626-19.942191 19.954471s8.928347 19.954471 19.942191 19.954471l84.264149 0 0 640.687918c0 60.479443 49.203632 109.683075 109.683075 109.683075l416.474366 0c60.479443 0 109.683075-49.203632 109.683075-109.683075L833.454246 209.385844l75.595722 0c11.012821 0 19.942191-8.940626 19.942191-19.954471S920.063813 169.476903 909.050991 169.476903zM376.2482 138.130987c0-17.593703 14.314007-31.907711 31.907711-31.907711l211.547067 0c17.593703 0 31.907711 14.314007 31.907711 31.907711l0 31.346939L376.2482 169.477926 376.2482 138.130987zM793.569864 850.074785c0 38.486546-31.312146 69.798692-69.798692 69.798692L307.297828 919.873478c-38.486546 0-69.798692-31.312146-69.798692-69.798692L237.499136 211.042577l556.070728 0L793.569864 850.074785z"
+                    p-id="2225"
+                  ></path>
+                  <path
+                    d="M510.662539 861.276918c11.012821 0 19.954471-8.92937 19.954471-19.942191L530.61701 294.912753c0-11.013845-8.94165-19.942191-19.954471-19.942191s-19.954471 8.928347-19.954471 19.942191L490.708068 841.334727C490.708068 852.347548 499.649717 861.276918 510.662539 861.276918z"
+                    p-id="2226"
+                  ></path>
+                  <path
+                    d="M374.562814 801.449321c11.012821 0 19.954471-8.92937 19.954471-19.942191L394.517285 354.74035c0-11.013845-8.94165-19.942191-19.954471-19.942191s-19.954471 8.928347-19.954471 19.942191l0 426.76678C354.608344 792.519951 363.549993 801.449321 374.562814 801.449321z"
+                    p-id="2227"
+                  ></path>
+                  <path
+                    d="M649.832182 801.449321c11.012821 0 19.954471-8.92937 19.954471-19.942191L669.786653 354.74035c0-11.013845-8.94165-19.942191-19.954471-19.942191s-19.954471 8.928347-19.954471 19.942191l0 426.76678C629.877711 792.519951 638.81936 801.449321 649.832182 801.449321z"
+                    p-id="2228"
+                  ></path>
+                </svg>
+              </span>
+            </template>
             <template slot-scope="{ row, index }" slot="action">
               <!-- 生产环境 -->
               <div v-if="isProduction">
@@ -175,6 +225,7 @@
                 >
                   <i class="fa fa-eye" aria-hidden="true"></i>
                 </span>
+                <i class="-dedent"></i>
               </div>
               <!-- 开发环境 -->
               <div v-else>
@@ -285,7 +336,9 @@ import {
   getReadme,
   logout,
   searchAll,
-  Upload
+  Upload,
+  UpdatePermission,
+  DeleteFile
 } from "@/API/api";
 import { checkFileType } from "../utils/index";
 import DPlayer from "../components/Dplayer";
@@ -353,6 +406,12 @@ export default {
         },
         {
           title: " ",
+          slot: "delete",
+          width: 50,
+          align: "center"
+        },
+        {
+          title: " ",
           slot: "action",
           width: 50,
           align: "center"
@@ -394,6 +453,7 @@ export default {
         name: "GONEList",
         page_title: "GONEList",
         upload: false,
+        admin_permission: false,
         version: "v0.0.0"
       }
     };
@@ -460,6 +520,40 @@ export default {
     }
   },
   methods: {
+    update_permission() {
+      let secret = "";
+      secret = window.sessionStorage.getItem("gonelist_secret");
+      if (secret === null) {
+        secret = window.prompt("请输入你的上传密钥：", "");
+        if (secret === "") return;
+      }
+      UpdatePermission(this.baseurl, secret).then(resp => {
+        if (resp.code === 200) {
+          this.site_config.admin_permission = true;
+          window.sessionStorage.setItem("gonelist_secret", secret);
+        } else {
+          this.$Message.error("token鉴权失败");
+          window.sessionStorage.removeItem("gonelist_secret");
+        }
+      });
+    },
+    delete_file(path, file_name) {
+      let b = window.confirm("确认删除该文件吗?\n" + file_name);
+      if (!b) return;
+      DeleteFile(
+        this.baseurl,
+        path,
+        sessionStorage.getItem("gonelist_secret")
+      ).then(resp => {
+        if (resp.code === 200) {
+          this.$Message.info("文件删除成功");
+          this.init();
+        }
+      });
+    },
+    mkdir(path) {
+      console.log(path);
+    },
     // 文件上传
     upload() {
       let secret = window.localStorage.getItem("gonelist_secret");
@@ -481,10 +575,10 @@ export default {
           param = param.slice(1);
         }
         console.log(e.target.files[0]);
-        if (e.target.files[0].size >= 4194304) {
-          this.$Message.error("当前仅支持文件小于4Mb");
-          return;
-        }
+        // if (e.target.files[0].size >= 4194304) {
+        //   this.$Message.error("当前仅支持文件小于4Mb");
+        //   return;
+        // }
         let body = new FormData();
         body.append("file", e.target.files[0]);
         Upload(
